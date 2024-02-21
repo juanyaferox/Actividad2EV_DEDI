62b22461c121ee035f76a9431e310ee3621b6439
juanya <juaniago2001@gmail.com>
2024-02-21 04:32:06 +0100

    Agregar archivos de proyecto.

---
+++ Models/Linea.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad2EV.Models
+{
+    internal class Linea
+    {
+        public Linea()
+        {
+        }
+        public Linea(int id, string municipioOr, string municipioDest, DateTime horaInic, DateTime intervaloBus)
+        {
+            Id = id;
+            MunicipioOr = municipioOr;
+            MunicipioDest = municipioDest;
+            HoraInic = horaInic;
+            IntervaloBus = intervaloBus;
+        }
+        public int Id { get; set; }
+        public string MunicipioOr { get; set; }
+        public string MunicipioDest { get; set; }
+        public DateTime HoraInic {  get; set; }
+        public DateTime IntervaloBus { get; set; } 
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Linea linea &&
+                   Id == linea.Id &&
+                   MunicipioOr == linea.MunicipioOr &&
+                   MunicipioDest == linea.MunicipioDest &&
+                   HoraInic == linea.HoraInic &&
+                   IntervaloBus == linea.IntervaloBus;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, MunicipioOr, MunicipioDest, HoraInic, IntervaloBus);
+        }
+    }
+}
---
+++ Models/Paradas.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad2EV.Models
+{
+    internal class Paradas
+    {
+        public Paradas()
+        {
+        }
+
+        public Paradas(int numLinea, string municipio, DateTime intervaloHS)
+        {
+            NumLinea = numLinea;
+            Municipio = municipio;
+            IntervaloHS = intervaloHS;
+        }
+
+        public int NumLinea { get; set; }
+        public String Municipio { get; set; }
+        public DateTime IntervaloHS { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Paradas paradas &&
+                   NumLinea == paradas.NumLinea &&
+                   Municipio == paradas.Municipio &&
+                   IntervaloHS == paradas.IntervaloHS;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(NumLinea, Municipio, IntervaloHS);
+        }
+    }
+
+}
